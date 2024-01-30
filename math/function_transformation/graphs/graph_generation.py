98e9d8f65abb28eebce09b2ba5c5a051fdd40bd2
ViliusP <viliuspaliokas@gmail.com>
2024-01-31 00:31:56 +0200

    sleep

--- math/function_transformation/graphs/graph_generation.py
+++ math/function_transformation/graphs/graph_generation.py
@@ -5,6 +5,7 @@ import shutil
 import matplotlib.patches as patches
 from matplotlib.patches import FancyArrowPatch
 from matplotlib.patches import Arrow
+import matplotlib.cm as cm
 
 GRAPHS_DIR = './math/function_transformation/graphs'
 
@@ -105,30 +106,37 @@ def plot_graph(domain, func, label, filename: str, tick_step=2, values_range=Non
         Y_full = func(X_full)
 
         # Determine the visible range based on values_range
-        y_min, y_max = values_range if values_range else (min(Y_full), max(Y_full))
+        y_min, y_max = values_range if values_range else (
+            min(Y_full), max(Y_full))
         ax.set_ylim(y_min, y_max)
 
         # Plot the main part of the function
-        ax.plot(X_full, Y_full, label=label, color='blue')  # Ensure main plot is blue
+        # Ensure main plot is blue
+        ax.plot(X_full, Y_full, label=label, color='blue')
 
         # Define Y-axis ticks
-        y_ticks = np.arange(y_min, y_max + 1, y_tick_step if y_tick_step is not None else tick_step)
+        y_ticks = np.arange(
+            y_min, y_max + 1, y_tick_step if y_tick_step is not None else tick_step)
 
         # Add dashed lines at the start and end if include_dashed_lines is True
         if include_dashed_lines:
             # Find indices of the visible points
-            visible_indices = np.where((Y_full >= y_min) & (Y_full <= y_max))[0]
+            visible_indices = np.where(
+                (Y_full >= y_min) & (Y_full <= y_max))[0]
             if len(visible_indices) > 0:
                 n_points = 40  # Number of points for the dashed lines
                 start_index = max(visible_indices[0] + n_points, 0)
-                end_index = min(visible_indices[-1] - n_points, len(X_full) - 1)
+                end_index = min(
+                    visible_indices[-1] - n_points, len(X_full) - 1)
 
                 # Draw dashed lines for the first and last n visible points
-                ax.plot(X_full[visible_indices[0]:start_index], Y_full[visible_indices[0]:start_index], linestyle='--', color='white', alpha=0.5)
-                ax.plot(X_full[end_index:visible_indices[-1]], Y_full[end_index:visible_indices[-1]], linestyle='--', color='white', alpha=0.5)
+                ax.plot(X_full[visible_indices[0]:start_index], Y_full[visible_indices[0]                        :start_index], linestyle='--', color='white', alpha=0.5)
+                ax.plot(X_full[end_index:visible_indices[-1]],
+                        Y_full[end_index:visible_indices[-1]], linestyle='--', color='white', alpha=0.5)
 
-        ax.set_yticks([tick for tick in y_ticks if y_min <= tick <= y_max and tick != 0])
-        
+        ax.set_yticks([tick for tick in y_ticks if y_min <=
+                      tick <= y_max and tick != 0])
+
         # Move left y-axis and bottom x-axis to zero
         ax.spines['bottom'].set_position('zero')
         ax.spines['left'].set_position('zero')
@@ -150,7 +158,8 @@ def plot_graph(domain, func, label, filename: str, tick_step=2, values_range=Non
                 ha='center', va='center')
 
         # Set X-axis ticks using the same tick step, exclude zero
-        ax.set_xticks([tick for tick in np.arange(domain[0], domain[1] + 1, tick_step) if tick != 0])
+        ax.set_xticks([tick for tick in np.arange(
+            domain[0], domain[1] + 1, tick_step) if tick != 0])
 
         # Display a single zero at the origin
         ax.text(-.5, -.75, '0', ha='center', va='center',
@@ -164,8 +173,13 @@ def plot_graph(domain, func, label, filename: str, tick_step=2, values_range=Non
         # Save the figure before showing it
         plt.savefig(f'{GRAPHS_DIR}/{filename}', bbox_inches='tight')
 
+
 def plot_combined_lines():
     with plt.xkcd():
+        plt.rcParams.update({
+            'grid.linewidth': 0.5,
+            'axes.grid': True,
+        })
 
         # Define the points
         points = [(-1, 1), (1, 1), (2, -2), (3, 0)]
@@ -173,19 +187,113 @@ def plot_combined_lines():
         # Create a figure and axis
         fig, ax = plt.subplots()
 
-        # Plot each line segment
-        for i in range(len(points) - 1):
+        # Get a colormap
+        cmap = cm.get_cmap('Blues')  # Sequential colormap
+
+        # Calculate the number of line segments
+        num_segments = len(points) - 1
+
+        # Plot each line segment with different shades
+        for i in range(num_segments):
             x_values = [points[i][0], points[i + 1][0]]
             y_values = [points[i][1], points[i + 1][1]]
-            ax.plot(x_values, y_values, marker='o')
+            # Adjust color selection to avoid very light colors
+            # Adjust range within colormap
+            color = cmap(0.7 + 0.4 * i / num_segments)
+            ax.plot(x_values, y_values, marker='o', color=color)
 
-        # Set labels and title (optional)
-        ax.set_xlabel('X-axis')
-        ax.set_ylabel('Y-axis')
-        ax.set_title('Combined Line Graph')
+        # Adjust axes and ticks
+        ax.set_xlim(-2, 4)  # Extend x-axis
+        ax.set_ylim(-3, 3)  # Extend y-axis
 
-    # Show the plot
-    plt.savefig(f'{GRAPHS_DIR}/exercise_4_plot', bbox_inches='tight')
+        # Set same spacing for both axes
+        ax.set_aspect('equal', adjustable='box')
+
+        # Set ticks to step of 1, remove zero, and the largest ticks
+        x_ticks = [tick for tick in ax.get_xticks() if tick != 0 and tick != 4]
+        y_ticks = [tick for tick in ax.get_yticks() if tick != 0 and tick != 3]
+        ax.set_xticks(x_ticks)
+        ax.set_yticks(y_ticks)
+
+        # Add zero label near the origin
+        ax.text(-0.1, -0.15, '0', ha='right', va='top')
+
+        # Add arrowheads
+        ax.plot((1), (0), ls="", marker=">", ms=10, color="k",
+                transform=ax.get_yaxis_transform(), clip_on=False, zorder=5)
+        ax.plot((0), (1), ls="", marker="^", ms=10, color="k",
+                transform=ax.get_xaxis_transform(), clip_on=False, zorder=5)
+
+        # Move left y-axis and bottom x-axis to zero
+        ax.spines['bottom'].set_position('zero')
+        ax.spines['left'].set_position('zero')
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+
+        # Save the plot
+        plt.savefig(f'{GRAPHS_DIR}/exercise_4_plot.pdf', bbox_inches='tight')
+
+
+def plot_quadratic_functions():
+    with plt.xkcd():
+        # Update the style for the grid
+        plt.rcParams.update({
+            'grid.linewidth': 0.5,
+            'axes.grid': True,
+        })
+
+        # Define the primary quadratic function f(x) = x^2
+        def f(x):
+            return (x-3)**2-1
+
+        # Define the translated quadratic function g(x) = (x + 2)^2 - 3
+        def g(x):
+            return -((x + 3)**2)+1
+
+     # Create the figure and axis
+        fig, ax = plt.subplots()
+
+        # Generate x values for both functions
+        x_values = np.linspace(-6, 6, 400)
+
+        # Plot the primary quadratic function f(x)
+        ax.plot(x_values, f(x_values), label='f(x)', color='blue')
+
+        # Plot the translated quadratic function g(x)
+        ax.plot(x_values, g(x_values), label='g(x)', color='orange')
+
+        # Improve label positioning
+        ax.text(2, f(2)+0.5, 'f(x)', fontsize=12, verticalalignment='bottom', horizontalalignment='left', color='blue')
+        ax.text(-5, g(-2)+0.5, 'g(x)', fontsize=12, verticalalignment='bottom', horizontalalignment='left', color='orange')
+
+        # Set ticks to step of 1, remove zero, and the largest ticks
+        ax.set_xticks([tick for tick in np.arange(-6, 7, 1) if tick != 0])
+        ax.set_yticks([tick for tick in np.arange(-5, 6, 1) if tick not in [0, 5]])
+
+        # Set aspect of the plot to be equal for both axes
+        ax.set_aspect('equal', adjustable='box')
+
+        # Set limits for x and y axes
+        ax.set_xlim(-6, 6)
+        ax.set_ylim(-5, 5)
+
+        # Add arrowheads
+        ax.plot((1), (0), ls="", marker=">", ms=10, color="k",
+                transform=ax.get_yaxis_transform(), clip_on=False, zorder=5)
+        ax.plot((0), (1), ls="", marker="^", ms=10, color="k",
+                transform=ax.get_xaxis_transform(), clip_on=False, zorder=5)
+
+        # Move left y-axis and bottom x-axis to zero
+        ax.spines['bottom'].set_position('zero')
+        ax.spines['left'].set_position('zero')
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+
+        # Add grid
+        ax.grid(True, which='both')
+
+        # Save the plot to a file
+        plt.savefig(f'{GRAPHS_DIR}/exercise_6_two_functions.pdf', bbox_inches='tight')
 
 
 # Pre
@@ -255,4 +363,7 @@ plot_graph(domain=(-6, 2), func=quadratic_formula(h=2),
            label='$f(x+2) = (x+2)^2$', filename='quadratic_func_transform_lshifted_2.pdf',  values_range=(-2, 15), y_tick_step=2)
 
 # Exercises
+# 4
 plot_combined_lines()
+# 6
+plot_quadratic_functions()
